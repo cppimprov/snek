5a763a337cc1d1e4ff792e4a337785bf5db41d5f
cppimprov <cppimprov@gmail.com>
2021-03-02 20:59:57 +0000

    Change object_handle to use std::unique_ptr internally.

--- Code/snek/snek_sdl_object_handle.hpp
+++ Code/snek/snek_sdl_object_handle.hpp
@@ -14,15 +14,7 @@ namespace snek
 		{
 		public:
 
-			object_handle();
-
-			object_handle(object_handle const&) = delete;
-			object_handle& operator=(object_handle const&) = delete;
-
-			object_handle(object_handle&& other);
-			object_handle& operator=(object_handle&& other);
-
-			~object_handle();
+			object_handle() = default;
 
 			bool is_valid() const;
 			void destroy();
@@ -37,47 +29,13 @@ namespace snek
 
 		private:
 
-			T* m_handle;
-			Deleter m_deleter;
+			std::unique_ptr<T, Deleter> m_handle;
 		};
 
 		template<class T, class D>
-		object_handle<T, D>::object_handle():
-			m_handle(nullptr) { }
-
-		template<class T, class D>
 		object_handle<T, D>::object_handle(T* object, D deleter):
-			object_handle()
-		{
-			reset(object, std::move(deleter));
-		}
+			m_handle(object, std::move(deleter)) { }
 		
-		template<class T, class D>
-		object_handle<T, D>::object_handle(object_handle&& other):
-			object_handle()
-		{
-			reset(other.m_handle, std::move(other.m_deleter));
-			other.destroy();
-		}
-
-		template<class T, class D>
-		object_handle<T, D>& object_handle<T, D>::operator=(object_handle&& other)
-		{
-			auto temp = std::move(other);
-
-			using std::swap;
-			swap(m_handle, temp.m_handle);
-			swap(m_deleter, temp.m_deleter);
-
-			return *this;
-		}
-
-		template<class T, class D>
-		object_handle<T, D>::~object_handle()
-		{
-			destroy();
-		}
-
 		template<class T, class D>
 		bool object_handle<T, D>::is_valid() const
 		{
@@ -87,17 +45,13 @@ namespace snek
 		template<class T, class D>
 		T* object_handle<T, D>::get_handle() const
 		{
-			return m_handle;
+			return m_handle.get();
 		}
 
 		template<class T, class D>
 		void object_handle<T, D>::destroy()
 		{
-			if (m_deleter)
-				m_deleter(m_handle);
-
-			m_handle = nullptr;
-			m_deleter = D();
+			m_handle.reset();
 		}
 		
 		template<class T, class D>
@@ -105,8 +59,7 @@ namespace snek
 		{
 			destroy();
 
-			m_handle = object;
-			m_deleter = std::move(deleter);
+			m_handle = { object, std::move(deleter) };
 		}
 
 	} // sdl
